108b3f5099b713b69c9a364be49d587a3514bd5d
oryemcollins <oryemnani@gmail.com>
2025-09-21 12:48:03 +0300

    chore(package): update package.json to add dev and start scripts, change module type, and include nodemon as a dev dependency

--- Server.js
+++ Server.js
@@ -1,6 +1,8 @@
 import express from 'express';
+import "dotenv/config";
 const app = express();
+const port = process.env.PORT || 8001;
 
-app.listen(5001, () => {
-  console.log('Server is running on port 5001');
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
